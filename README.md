8abbf118d6a176badb6b59e4b319c1cb040d4f6f
jianxun <medusa.leee@gmail.com>
2018-02-18 16:38:17 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 12306-captcha-viewer
-12306验证码渲染
+
+> 12306验证码渲染GUI
+
+### 效果
+
+![示例](./resource/ex.png)
---
+++ src/main/java/com/jianxun/App.java
@@ -0,0 +1,44 @@
+package com.jianxun;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class ImageViewer extends Frame {
+    BufferedImage bufferedImage = null;
+    public ImageViewer(BufferedImage bufferedImage) {
+        this.bufferedImage = bufferedImage;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(this.bufferedImage, 0, 20, null);
+    }
+
+    public void launchFrame() {
+        setSize(293, 210);
+        setLocation(100, 100);
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
+
+public class App{
+    public static void main(String[] args ) throws IOException {
+        //BufferedImage bufferedImage = ImageIO.read(System.in);
+        String path = "/Users/jianxun/Downloads/captcha-image.jpeg";
+        BufferedImage bufferedImage = ImageIO.read(new FileInputStream(path));
+        ImageViewer iv = new ImageViewer(bufferedImage);
+        iv.launchFrame();
+    }
+}
