7dc1bdde4cfaecd652998e4c34e47435d041b1b1
jianxun <medusa.leee@gmail.com>
2018-02-18 18:17:11 +0800

    使用编号标记图片.

--- src/main/java/com/jianxun/App.java
+++ src/main/java/com/jianxun/App.java
@@ -16,11 +16,24 @@ class ImageViewer extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(this.bufferedImage, 0, 20, null);
+        Font f = new Font("宋体", Font.BOLD, 15);
+        g.setFont(f);
+        g.drawString("请在命令行中输入正确答案的图片编号。", 0, 40);
+        g.drawString("多个用逗号\",\"隔开，如:1,3。", 0, 60);
+        g.drawString("输入完成后关闭此窗口。", 0, 80);
+        g.drawImage(this.bufferedImage, 0, 100, null);
+        g.drawString("1", 30, 135);
+        g.drawString("2", 105, 135);
+        g.drawString("3", 180, 135);
+        g.drawString("4", 245, 135);
+        g.drawString("5", 30, 295);
+        g.drawString("6", 105, 295);
+        g.drawString("7", 180, 295);
+        g.drawString("8", 245, 295);
     }
 
     public void launchFrame() {
-        setSize(293, 210);
+        setSize(293, 350);
         setLocation(100, 100);
         setVisible(true);
 
